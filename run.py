d61fb4273806c2999cb04ffd60543d5326ac9971
Prabuddha Kumar Dwivedi <76592429+Prabuddhadev@users.noreply.github.com>
2023-10-22 01:55:20 +0530

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
-api.add_resource(ConstructionDataController, '/suppliers', '/<vars>', methods=['GET', 'POST'])
+api.add_resource(ConstructionDataController, '/<vars>', methods=['GET', 'POST'])
 
 if __name__ == '__main__':
     app.run(debug=True)
