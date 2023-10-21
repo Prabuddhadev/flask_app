664bf5230337b5a36172c1fe76eb005eaa793986
Prabuddha Kumar Dwivedi <76592429+Prabuddhadev@users.noreply.github.com>
2023-10-22 01:39:35 +0530

    Update run.py

--- run.py
+++ run.py
@@ -9,5 +9,5 @@ CORS(app)
 api.add_resource(ConstructionDataController, '/suppliers', '/<vars>', methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(debug=True)
 
